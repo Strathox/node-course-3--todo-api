a97b20ea2682cd57ee395611d4b38f317a96a6d2
Strathox <benchude@gmail.com>
2018-03-14 06:28:52 +0100

    Init commit

---
+++ mongodb-connect.js
@@ -0,0 +1,23 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = ObjectID();
+console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').find({
+    name: "Benji"
+  }).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs,undefined,2));
+  }, (err) => {
+    console.log('Unable to fetch todos');
+  });
+
+  db.close();
+});
