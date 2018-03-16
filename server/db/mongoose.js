96d8be5331c6677f9e097fbb18b485dfe7e20ed6
Strathox <benchude@gmail.com>
2018-03-16 12:26:23 +0100

    Corrected error in MONGOLAB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 
 process.env.MONGOLAB_URI = 'mongodb://benchude:TangentGPE2001@ds215089.mlab.com:15089/node-todo-api'
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp2');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp2');
 
 module.exports = {mongoose};
